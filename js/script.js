210eddbfce336fb9b509ad821005c8d827a80463
JanhaviPorwal <janhavi.official.22@gmail.com>
2022-11-07 12:22:39 +0530

    Adding images in dark mode

--- js/script.js
+++ js/script.js
@@ -13,8 +13,8 @@ icon.onclick = function() {
   document.body.classList.toggle("dark-theme");
 
   if(document.body.classList.contains("dark-theme")) {
-    icon.src = "img/sun.png";
+    icon.src = "./img/sun.png";
   } else {
-    icon.src = "img/moon.png";
+    icon.src = "./img/moon.png";
   }
 }
