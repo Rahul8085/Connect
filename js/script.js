5ff06bb8bbe4af4fab97d54c3354a49485b0d19e
Rahul Panchal <48758073+Rahul8085@users.noreply.github.com>
2022-11-07 11:31:31 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing", {
+    strings: ["an Engineer", "Front-End Developer", "Database Developer", "Blogger"],
+    typeSpeed: 90,
+    backSpeed: 40,
+    cursorChar: " |",
+    smartBackspace: true,
+    loop: true,
+  });
